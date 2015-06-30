659346451d706951754aff13d0286b90f079e8b1
Xinchun Tian <tianxc@gmail.com>
2015-06-29 21:34:11 -0400

    Tidy my repo

---
+++ Exploratory_Data_Analysis/ExData_Plotting1/plot4.R
@@ -0,0 +1,28 @@
+library(sqldf)
+if (!file.exists("household_power_consumption.txt")) {
+        if (!file.exists("household_power_consumption.zip")) {
+                url="https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+                download.file(url, destfile="./household_power_consumption.zip", method="curl")
+                dateDownloaded <- date()
+                unzip("household_power_consumption.zip")
+         } else {
+                unzip("household_power_consumption.zip")
+         }
+}
+png(file="plot4.png",width=480,height=480)
+df <- read.csv.sql("household_power_consumption.txt",sql="select * from file where Date in ('1/2/2007','2/2/2007')",header=TRUE,sep=";")
+df$datetime <- paste(df$Date, df$Time)
+df$datetime <- strptime(df$datetime, format = "%d/%m/%Y %H:%M:%S")
+df$weekdays <- as.factor(weekdays(df$datetime))
+par(mfcol=c(2,2))
+with(df, {
+    plot(df$datetime, Global_active_power, xlab="", ylab="Global Active Power", type="l")
+    plot(df$datetime, Sub_metering_1, main="", type = "n", xlab="", ylab="Energy sub metering")
+    with(subset(df,select=Sub_metering_1), points(df$datetime, df$Sub_metering_1, type="l"))
+    with(subset(df,select=Sub_metering_2), points(df$datetime, df$Sub_metering_2, type="l", col="red"))
+    with(subset(df,select=Sub_metering_3), points(df$datetime, df$Sub_metering_3, type="l", col="blue"))
+    legend("topright", col=c("black","blue","red"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lwd=2)
+    plot(datetime, Voltage, xlab="datetime", ylab="Voltage",type="l")
+    plot(datetime, Global_reactive_power, xlab="datetime", type="l")    
+})
+dev.off()
