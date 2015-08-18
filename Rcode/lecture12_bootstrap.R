0061235c0de6829064f92dec29328a31031fa7d2
Xinchun Tian <tianxc@gmail.com>
2015-08-17 20:06:10 -0400

    Remove wrongly added files.

--- Rcode/lecture12_bootstrap.R
+++
@@ -1,4 +0,0 @@
-##if you haven't installed the
-##package, uncomment this line
-##you only have to do this once
-##install.packages("bootstrap")
--- Rcode/p1mp.R
+++
@@ -1,2 +0,0 @@
-p <- seq(0, 1, length = 1000)
-plot(p, p * (1 - p), frame = FALSE, type = "l", lwd = 3)
